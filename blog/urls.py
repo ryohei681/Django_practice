8ad04cd47948593b142793b393044cfd930b52c5
Ryohei Tsunoda <83001827+ryohei681@users.noreply.github.com>
2021-09-16 16:22:50 +0900

    基本の書き換えまで作成

--- blog/urls.py
+++ blog/urls.py
@@ -21,4 +21,34 @@ urlpatterns = [
         #URLパターンの名前を'blog_detail'にする
         name='blog_detail'
     ),
+
+    # scienceカテゴリの一覧ページのURLパターン
+    path(
+        # scienceカテゴリの一覧ページのURLは「science-list/」
+        'science-list/',
+        # viewsモジュールのScienceViewを実行
+        views.ScienceView.as_view(),
+        # URLパターンの名前を'science_list'にする
+        name='science_list'
+    ),
+
+    # dailylifeカテゴリの一覧ページのURLパターン
+    path(
+        # dailylifeカテゴリの一覧ページのURLは「science-list/」
+        'dailylife-list/',
+        # viewsモジュールのDailylifeViewを実行
+        views.DailylifeView.as_view(),
+        # URLパターンの名前を'dailylife_list'にする
+        name='dailylife_list'
+    ),
+
+    # musicカテゴリの一覧ページのURLパターン
+    path(
+        # musicカテゴリの一覧ページのURLは「music-list/」
+        'music-list/',
+        # viewsモジュールのMusicViewを実行
+        views.MusicView.as_view(),
+        # URLパターンの名前を'music_list'にする
+        name='music_list'
+    ),
 ]
--- blog/views.py
+++ blog/views.py
@@ -34,6 +34,8 @@ class IndexView(ListView):
     context_object_name = 'orderby_records'
     #モデルBlogPostのオブジェクトにorder_by()を適用してBlogPostのレコードを投稿日時の降順で並び替える
     queryset = BlogPost.objects.order_by('-posted_at')
+    # 1ページに表示するレコードの件数
+    paginate_by = 2
 
 class BlogDetail(DetailView):
     '''詳細ページのビュー
@@ -46,3 +48,51 @@ class BlogDetail(DetailView):
     template_name ='post.html'
     # クラス変数modelにモデルBlogPostを設定
     model = BlogPost
+
+class ScienceView(ListView):
+    '''科学(science)カテゴリの記事を一覧表示するビュー
+    
+    '''
+    # science_list.htmlをレンダリングする
+    template_name = 'science_list.html'
+    # クラス変数modelにモデルBlogPostを設定
+    model = BlogPost
+    # object_listキーの別名を設定
+    context_object_name = 'science_records'
+    # cotegory='scienceのレコードを抽出して投稿日時の降順で並び替える
+    queryset = BlogPost.objects.filter(category='science').order_by('-posted_at')
+    # 1ページの表示するレコードの件数
+    paginate_by = 2
+
+class DailylifeView(ListView):
+    '''日常(dailylife)カテゴリの記事を一覧表示するビュー
+    
+    '''
+    # dailylife_list.htmlをレンダリングする
+    template_name = 'dailylife_list.html'
+    # クラス変数modelにモデルBlogPostを設定
+    model = BlogPost
+    # object_listキーの別名を設定
+    context_object_name = 'dailylife_records'
+    # cotegory='dailylife'のレコードを抽出して投稿日時の降順で並び替える
+    queryset = BlogPost.objects.filter(category='dailylife').order_by('-posted_at')
+    # 1ページの表示するレコードの件数
+    paginate_by = 2
+
+class MusicView(ListView):
+    '''音楽(music)カテゴリの記事を一覧表示するビュー
+    
+    '''
+    # music_list.htmlをレンダリングする
+    template_name = 'music_list.html'
+    # クラス変数modelにモデルBlogPostを設定
+    model = BlogPost
+    # object_listキーの別名を設定
+    context_object_name = 'music_records'
+    # cotegory='music'のレコードを抽出して投稿日時の降順で並び替える
+    queryset = BlogPost.objects.filter(category='music').order_by('-posted_at')
+    # 1ページの表示するレコードの件数
+    paginate_by = 2
+
+    
+    
